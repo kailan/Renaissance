62931f5c2dadc6e7ebfc17dc464149c0fcee3232
molenzwiebel <info@thijsmolendijk.nl>
2015-12-22 15:58:51 +0100

    Add debugging command that runs JavaScript

--- src/main/kotlin/net/hungerstruck/renaissance/RenaissancePlugin.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/RenaissancePlugin.kt
@@ -1,6 +1,11 @@
 package net.hungerstruck.renaissance
 
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
+import javax.script.ScriptEngine
 
 /**
  * Bukkit entry point for Renaissance.
@@ -8,7 +13,24 @@ import org.bukkit.plugin.java.JavaPlugin
  * Created by molenzwiebel on 22-12-15.
  */
 class RenaissancePlugin : JavaPlugin() {
+    private val sessions: MutableMap<CommandSender, ScriptEngine> = hashMapOf()
+    private val nashornEngine: NashornScriptEngineFactory = NashornScriptEngineFactory()
+
     override fun onEnable() {
         Renaissance.initialize(this)
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        val engine = if (sessions[sender] != null) {
+            sessions[sender]!!
+        } else {
+            sessions[sender] = nashornEngine.scriptEngine
+            sessions[sender]!!
+        }
+
+        val res = engine.eval(args.joinToString(" "))
+
+        sender.sendMessage("${ChatColor.GRAY}> ${ChatColor.WHITE}$res")
+        return true
     }
 }
