54765272435d9e71be5ac2e1c66faffd80d68a5e
Kailan Blanks <kailan@kmp.pw>
2015-12-22 16:49:14 +0000

    Add config options

--- src/main/kotlin/net/hungerstruck/renaissance/config/RConfig.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/config/RConfig.kt
@@ -29,12 +29,32 @@ object RConfig {
 
     object Lobby {
         var autoStart: Boolean by path("lobby.auto-start", true)
-        var minimumPlayerStartCount: Int by path("lobby.minimum-player-start-count", 4)
-        var maximumPlayerStartCount: Int by path("lobby.maximum-player-start-count", 24)
+        var minimumPlayerStartCount: Int by path("lobby.minimum-players", 2)
+        var maximumPlayerStartCount: Int by path("lobby.maximum-players", 24)
+        // needs the countdown
 
         var spawnLocation: Location by path("lobby.spawn-vector", {
             val parts = it.split(",")
             Vector(parts[1].toInt(), parts[2].toInt(), parts[3].toInt()).toLocation(Bukkit.getWorld(parts[0]))
         }, { "${it.world.name},${it.blockX},${it.blockY},${it.blockZ}" })
+    }
+
+    object Match {
+        // not sure how to do the countdowns
+    }
+
+    object Thirst {
+        var refreshRate: Int by path("thirst.refresh-rate", 250)
+        var thirstDecrement: Int by path("thirst.thirst-decrement", 1)
+        var dehydrationLevel: Int by path("thirst.dehydration-level", 10)
+        var healthDamage: Int by path("thirst.health-damage", 1)
+        var hungerDamage: Int by path("thirst.hunger-damage", 0)
+
+        var waterBottleRegen: Int by path("thirst.container-settings.waterbottle-regened", 100)
+        var waterBucketRegen: Int by path("thirst.container-settings.waterbucket-regened", 100)
+    }
+
+    object Chat {
+        var radius: Int by path("chat.radius", 30)
     }
 }
