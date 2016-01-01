dc22bf285e9b36cbf7f92895cc6598a2a6408766
molenzwiebel <info@thijsmolendijk.nl>
2016-01-01 13:19:38 +0100

    Make some Countdown methods optional

--- src/main/kotlin/net/hungerstruck/renaissance/countdown/Countdown.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/countdown/Countdown.kt
@@ -4,8 +4,8 @@ package net.hungerstruck.renaissance.countdown
  * Created by molenzwiebel on 01-01-16.
  */
 abstract class Countdown {
-    abstract fun onStart(timeLeft: Int)
+    open fun onStart(timeLeft: Int) {}
     abstract fun onTick(timeLeft: Int)
     abstract fun onFinish()
-    abstract fun onCancel()
+    open fun onCancel() {}
 }
