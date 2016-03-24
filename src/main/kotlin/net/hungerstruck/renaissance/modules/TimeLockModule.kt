b68c3306c02ba857e2e0a6ddbc58a3ed60537513
skreem <cskreem@gmail.com>
2016-03-24 16:59:53 -0400

    Rework some time logic to be more concise

--- src/main/kotlin/net/hungerstruck/renaissance/modules/TimeLockModule.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/modules/TimeLockModule.kt
@@ -1,11 +1,13 @@
 package net.hungerstruck.renaissance.modules
 
+import net.hungerstruck.renaissance.RPlayer
 import net.hungerstruck.renaissance.event.match.RMatchLoadEvent
 import net.hungerstruck.renaissance.event.match.RMatchStartEvent
 import net.hungerstruck.renaissance.match.RMatch
 import net.hungerstruck.renaissance.xml.module.RModule
 import net.hungerstruck.renaissance.xml.module.RModuleContext
 import net.hungerstruck.renaissance.xml.toBool
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.jdom2.Document
 
@@ -21,8 +23,11 @@ class TimeLockModule(match: RMatch, document: Document, modCtx: RModuleContext) 
     }
 
     @EventHandler
-    public fun onMatchLoad(event: RMatchLoadEvent) {
+    public fun onMatchStartEvent(event: RMatchStartEvent) {
+        if (!isMatch(event.match)) return
+
         // Set dayLightCycle to the opposite of the value from XML (if mapmaker wants it to be locked, true, then cycling should be off, false)
         event.match.world.setGameRuleValue("doDaylightCycle", locked.not().toString());
+
     }
 }
--- src/main/kotlin/net/hungerstruck/renaissance/modules/TimeSetModule.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/modules/TimeSetModule.kt
@@ -4,6 +4,7 @@ import net.hungerstruck.renaissance.Renaissance
 import net.hungerstruck.renaissance.event.match.RMatchLoadEvent
 import net.hungerstruck.renaissance.event.match.RMatchStartEvent
 import net.hungerstruck.renaissance.match.RMatch
+import net.hungerstruck.renaissance.xml.module.Dependencies
 import net.hungerstruck.renaissance.xml.module.RModule
 import net.hungerstruck.renaissance.xml.module.RModuleContext
 import net.hungerstruck.renaissance.xml.toLong
@@ -14,9 +15,7 @@ import org.bukkit.scheduler.BukkitScheduler
 import org.bukkit.scheduler.BukkitTask
 import org.jdom2.Document
 
-/**
- * Created by molenzwiebel on 21-12-15.
- */
+@Dependencies(TimeLockModule::class)
 class TimeSetModule(match: RMatch, document: Document, modCtx: RModuleContext) : RModule(match, document, modCtx) {
     val value: Long
 
@@ -27,11 +26,9 @@ class TimeSetModule(match: RMatch, document: Document, modCtx: RModuleContext) :
 
     @EventHandler
     public fun onMatchLoad(event: RMatchLoadEvent) {
-         event.match.world.setTime(value)
-    }
-
-    @EventHandler
-    public fun onMatchStart(event: RMatchStartEvent) {
+        if (!isMatch(event.match)) return
+        
         event.match.world.setTime(value)
+        event.match.world.setGameRuleValue("doDaylightCycle", "false"); // stop time during load, will be set to true if the lock module determines it should be
     }
 }
