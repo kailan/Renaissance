7407762a90e6a285881c62351955ed1880104c92
skreem <cskreem@gmail.com>
2016-04-03 10:44:11 -0400

    Fix odd formatting in commands

--- src/main/kotlin/net/hungerstruck/renaissance/commands/MapCommands.kt
+++ src/main/kotlin/net/hungerstruck/renaissance/commands/MapCommands.kt
@@ -1,10 +1,8 @@
 package net.hungerstruck.renaissance.commands
 
-import com.sk89q.minecraft.util.commands.Command;
-import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.Command
+import com.sk89q.minecraft.util.commands.CommandContext
 import com.sk89q.minecraft.util.commands.CommandException
-import com.sk89q.minecraft.util.commands.CommandPermissions;
-import kotlin.jvm.JvmStatic;
 import net.hungerstruck.renaissance.rplayer
 import net.hungerstruck.renaissance.xml.RMap
 import net.hungerstruck.renaissance.xml.RMapInfo
@@ -20,20 +18,20 @@ object MapCommands {
         if (player.rplayer.match == null && player.rplayer.lobby == null)
             throw CommandException("You must be in a match or lobby to execute this command")
         val map: RMap = if (player.rplayer.match != null) CommandUtils.getPlayer(sender).rplayer.match!!.map else CommandUtils.getPlayer(sender).rplayer.lobby!!.lobbyMap
-        val mapInfo: RMapInfo = map.mapInfo;
+        val mapInfo: RMapInfo = map.mapInfo
 
-        sender.sendMessage(CommandUtils.formatHeader(ChatColor.DARK_AQUA.toString() + mapInfo.name + " " + ChatColor.GRAY + mapInfo.version));
-        sender.sendMessage(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Objective: " + ChatColor.RESET + ChatColor.GOLD + mapInfo.objective);
-        sender.sendMessage(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Author(s): ${ChatColor.RESET.toString() + ChatColor.GOLD.toString() + mapInfo.authors.map { ChatColor.DARK_AQUA.toString() + it.name }.joinToString(", ")}");
+        sender.sendMessage(CommandUtils.formatHeader(ChatColor.DARK_AQUA.toString() + mapInfo.name + " " + ChatColor.GRAY + mapInfo.version))
+        sender.sendMessage(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Objective: " + ChatColor.RESET + ChatColor.GOLD + mapInfo.objective)
+        sender.sendMessage(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Author(s): ${ChatColor.RESET.toString() + ChatColor.GOLD.toString() + mapInfo.authors.map { ChatColor.DARK_AQUA.toString() + it.name }.joinToString(", ")}")
 
         if (mapInfo.contributors.size > 0)
-            sender.sendMessage(ChatColor.DARK_PURPLE.toString() + "Contributor(s): ${mapInfo.contributors.map { ChatColor.DARK_AQUA.toString() + it.name }.joinToString(", ")}");
+            sender.sendMessage(ChatColor.DARK_PURPLE.toString() + "Contributor(s): ${mapInfo.contributors.map { ChatColor.DARK_AQUA.toString() + it.name }.joinToString(", ")}")
 
-        val rules: List<String> = mapInfo.rules;
+        val rules: List<String> = mapInfo.rules
         if (rules.size > 0) {
-            sender.sendMessage(ChatColor.DARK_PURPLE.toString() + "Rules:");
+            sender.sendMessage(ChatColor.DARK_PURPLE.toString() + "Rules:")
             for (i in rules.indices)
-                sender.sendMessage("${(i + 1)}) " + ChatColor.GOLD + rules.get(i));
+                sender.sendMessage("${(i + 1)}) " + ChatColor.GOLD + rules.get(i))
         }
     }
 }
